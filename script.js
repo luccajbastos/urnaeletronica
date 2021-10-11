731f5a12731e19ade327ad526714528fcfee7343
Lucca <luccajbastos@gmail.com>
2021-10-11 15:12:33 -0300

    alpha

---
+++ script.js
@@ -0,0 +1,8 @@
+const qs = (o) => document.querySelector(o);
+
+let seuVotoPara = qs('.d-1-1 span');
+let cargo = qs('.d-1-2 span');
+let descricao = qs('.d-1-4');
+let aviso = qs('.d-2');
+let lateral = qs('.d-1-right');
+let numeros = qs('.d-1-3');
